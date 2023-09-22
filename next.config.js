004d1d7a8f0913aed07cbeeb64cdddec7ecfdf1d
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-09-22 15:48:30 +0900

    수정

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 // module.exports = nextConfig
 
 // const isProduction = process.env.NODE_ENV === "production";
-// 주석담
+
 /** @type {import('next').NextConfig} */
 const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
@@ -16,7 +16,7 @@ const nextConfig = {
       {
         source: "/",
         destination: "/pages/test1",
-        permanent: false,
+        permanent: true,
       },
     ];
   },
